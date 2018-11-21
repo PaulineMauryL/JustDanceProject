5ce17fbc608c58ad3d7a0b64e2f64761534e5da3
audrey.jordan@epfl.ch <43436830+aujordan@users.noreply.github.com>
2018-11-21 16:20:37 +0100

    Avancee par Audrey

---
+++ mobile/src/main/java/com/pauli/justdanceproject/DanceActivity.java
@@ -0,0 +1,87 @@
+package com.pauli.justdanceproject;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class DanceActivity extends AppCompatActivity {
+
+    private MediaPlayer mySound = null;
+    private int Idmusictodisplay = 0;
+    private Boolean resume = true;
+    private ImageView imageButtonView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dance);
+
+        Bundle bunble = getIntent().getExtras();
+        if (bunble!=null){
+            Idmusictodisplay = bunble.getInt("musicchosen");
+            mySound = MediaPlayer.create(this, Idmusictodisplay);
+            mySound.start();
+            resume = false;
+        }
+    }
+
+    public void ResumeMusic(View view) {
+        Button pauseButton = findViewById(R.id.ResumeButton);
+        if (mySound !=null){
+            if (resume){
+                mySound.start();
+                resume = false;
+                pauseButton.setText(R.string.Resume);
+            }else {
+                mySound.pause();
+                resume = true;
+                pauseButton.setText(R.string.Restart);
+            }
+        }
+
+    }
+
+    public void UpButton(View view) {
+        if (!resume){
+            imageButtonView = findViewById(R.id.UpView);
+            ToggleFilter();
+        }
+    }
+
+
+    public void MiddleButton(View view) {
+        if (!resume){
+            imageButtonView = findViewById(R.id.MiddleView);
+            ToggleFilter();
+        }
+    }
+
+    public void DownButton(View view) {
+        if (!resume){
+            imageButtonView = findViewById(R.id.BottomView);
+            ToggleFilter();
+        }
+    }
+    final Runnable r1 = new Runnable() {
+        public void run() {
+            imageButtonView.setActivated(true);
+            imageButtonView.setEnabled(true);
+        }
+    };
+    final Runnable r2 = new Runnable() {
+        public void run() {
+            imageButtonView.setActivated(false);
+            imageButtonView.setEnabled(false);
+        }
+    };
+
+    public void ToggleFilter(){
+        imageButtonView.setActivated(true);
+        imageButtonView.setEnabled(false);
+        imageButtonView.postDelayed(r1,600);
+        imageButtonView.postDelayed(r2,1200);
+    }
+}
--- mobile/src/main/java/com/pauli/justdanceproject/MainActivity.java
+++ mobile/src/main/java/com/pauli/justdanceproject/MainActivity.java
@@ -1,13 +1,39 @@
 package com.pauli.justdanceproject;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_MUSIC = 1;
+    private int chosenMusic=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartDance(View view) {
+        if(chosenMusic != 0){
+            Intent intentStartDance = new Intent(MainActivity.this,DanceActivity.class);
+            intentStartDance.putExtra("musicchosen",chosenMusic);
+            startActivity(intentStartDance);
+        }else {
+            TextView textView =  findViewById(R.id.StartDanceText);
+            textView.setText("You need to choose a music first!");
+        }
+    }
+
+
+    public void ChooseCreuse(View view) {
+        chosenMusic = R.raw.creuser;
+    }
+    public void ChooseHercule(View view) {
+        chosenMusic = R.raw.zero;
     }
 }
