be2787048daeca38033aa966392308c4b589014a
audrey.jordan@epfl.ch <43436830+aujordan@users.noreply.github.com>
2019-01-07 16:48:12 +0100

    Progress bar and scoring using buttons

--- mobile/src/main/java/com/pauli/justdanceproject/DanceActivity.java
+++ mobile/src/main/java/com/pauli/justdanceproject/DanceActivity.java
@@ -3,10 +3,14 @@ package com.pauli.justdanceproject;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class DanceActivity extends AppCompatActivity {
 
@@ -18,9 +22,29 @@ public class DanceActivity extends AppCompatActivity {
     private Boolean resume = true;
     private ImageView imageButtonView = null;
     private int index = 0;
+    private int nextposition = 0;
     private int askedposition = 0;
     private int actualposition = 0;
     private int score = 0;
+    private Button button1=null;
+    private Button button2=null;
+    private Button button3=null;
+    private static final String PROGRESS_BAR_INCREMENT="ProgreesBarIncrementId";
+    static ProgressBar bar;
+    /**     * L'AtomicBoolean qui gère la destruction de la Thread de background     */
+    AtomicBoolean isRunning = new AtomicBoolean(false);
+    /**     * L'AtomicBoolean qui gère la mise en pause de la Thread de background     */
+    AtomicBoolean isPausing = new AtomicBoolean(false);
+    static Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int progress=msg.getData().getInt(PROGRESS_BAR_INCREMENT);
+            // Incrémenter la ProgressBar, on est bien dans la Thread de l'IHM
+            bar.incrementProgressBy(progress);
+            // On peut faire toute action qui met à jour l'IHM
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +58,52 @@ public class DanceActivity extends AppCompatActivity {
             musical.musicsound.start();
             mHandler = new Handler();
             resume = false;
+            index = 0;
+            score = 0;
+            askedposition = 0;
+            actualposition = 100;
+            bar = (ProgressBar) findViewById(R.id.progress);
+            bar.setMax(210);
         }
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        index = 0;
-        score = 0;
         mHandler.postDelayed(startMusic,1000);
+        // initialisation de la ProgressBar
+        bar.setProgress(0);
+        // Définition de la Thread (peut être effectuée dans une classe externe ou interne)
+        Thread background = new Thread(progressRunnable);
+        //Lancement de la Thread
+        isPausing.set(true);
+        isRunning.set(true);
+        background.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isPausing.set(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isPausing.set(false);
     }
 
     public void movementsOnMusic(){
         int i=0;
         int delay=0;
         i=index*2;
+        //Initialisation des AtomicBooleans
+        isPausing.set(false);
         while(i<musical.musictiming.length){
             delay = musical.musictiming[i];
             mHandler.postDelayed(r1,delay-musical.musicsound.getCurrentPosition());
-            mHandler.postDelayed(r2,delay-musical.musicsound.getCurrentPosition()+300);
-            mHandler.postDelayed(r3,delay-musical.musicsound.getCurrentPosition()+600);
+            mHandler.postDelayed(r2,delay-musical.musicsound.getCurrentPosition()+600);
+            mHandler.postDelayed(r3,delay-musical.musicsound.getCurrentPosition()+800);
             i=i+2;
         }
     }
@@ -66,17 +116,20 @@ public class DanceActivity extends AppCompatActivity {
                 resume = false;
                 pauseButton.setText(R.string.Resume);
                 movementsOnMusic();
+                isPausing.set(false);
             }else {
                 musical.musicsound.pause();
                 mHandler.removeCallbacks(r1);
                 mHandler.removeCallbacks(r2);
                 mHandler.removeCallbacks(r3);
                 resume = true;
+                isPausing.set(true);
                 pauseButton.setText(R.string.Restart);
             }
         }
 
     }
+
 
     public void UpButton(View view) {
         if (!resume){
@@ -99,10 +152,55 @@ public class DanceActivity extends AppCompatActivity {
             actualposition = 3;
         }
     }
+
+    private Runnable progressRunnable = new Runnable() {
+        /**
+         * Le Bundle qui porte les données du Message et sera transmis au Handler
+         */
+        Bundle messageBundle=new Bundle();
+        /**
+         * Le message échangé entre la Thread et le Handler
+         */
+        Message myMessage;
+        // Surcharge de la méthode run
+        public void run() {
+            try {
+                // Si isRunning est à false, la méthode run doit s'arrêter
+                while (isRunning.get()) {
+                    // Si l'activité est en pause mais pas morte
+                    while (isPausing.get() && (isRunning.get())) {
+                        // Faire une pause ou un truc qui soulage le CPU (dépend du traitement)
+                        Thread.sleep(2000);
+                    }
+                    // Effectuer le traitement, pour l'exemple je dors une seconde
+                    Thread.sleep(100);
+                    // Envoyer le message au Handler (la méthode handler.obtainMessage est plus efficace
+                    // que créer un message à partir de rien, optimisation du pool de message du Handler)
+                    //Instanciation du message (la bonne méthode):
+                    myMessage=handler.obtainMessage();
+                    //Ajouter des données à transmettre au Handler via le Bundle
+                    if(askedposition == actualposition) {
+                        messageBundle.putInt(PROGRESS_BAR_INCREMENT,10);
+                    }else if(nextposition == actualposition){
+                        messageBundle.putInt(PROGRESS_BAR_INCREMENT,5);
+                    }else{
+                        messageBundle.putInt(PROGRESS_BAR_INCREMENT,0);
+                    }
+                    //Ajouter le Bundle au message
+                    myMessage.setData(messageBundle);
+                    //Envoyer le message
+                    handler.sendMessage(myMessage);
+                }
+            } catch (Throwable t) {
+                // gérer l'exception et arrêter le traitement
+            }
+        }
+    };
+
     final Runnable r1 = new Runnable() {
         public void run() {
-            askedposition = musical.musictiming[index*2+1];
-            switch (askedposition){
+            nextposition = musical.musictiming[index*2+1];
+            switch (nextposition){
                 case 1:
                     imageButtonView = findViewById(R.id.UpView);
                     break;
@@ -115,29 +213,33 @@ public class DanceActivity extends AppCompatActivity {
             }
             imageButtonView.setActivated(true);
             imageButtonView.setEnabled(true);
+
+
+            button2 = findViewById(R.id.DownButton);
+            button2.setText("actualposition "+actualposition);
+
+
+            button1 = findViewById(R.id.MiddleButton);
+            button1.setText("nextposition "+nextposition);
         }
     };
     final Runnable r2 = new Runnable() {
         public void run() {
-
+            askedposition = nextposition;
             imageButtonView.setActivated(true);
             imageButtonView.setEnabled(false);
 
-            getPosition();
+            button2 = findViewById(R.id.UpButton);
+            button2.setText("askedposition "+askedposition);
         }
     };
-    private void getPosition() {
-        if (actualposition==askedposition)
-        {
-            score = score+1;
-        }
-        Button ButtonView=findViewById(R.id.MiddleButton);
-        ButtonView.setText(""+score);
-    }
+
     final Runnable r3 = new Runnable() {
         public void run() {
             imageButtonView.setActivated(false);
             index = index+1;
+            nextposition = 0;
+            askedposition = 0;
         }
     };
 
